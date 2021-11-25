be9fe9bb42bfcab320b135436bc914596661006f
Manoj Kumawat <manoj@internshala.com>
2021-11-25 16:41:44 +0530

    Added port number

--- helpers/database/config.js
+++ helpers/database/config.js
@@ -4,7 +4,8 @@ var connection = mysql.createConnection({
     host: "myclass.cpu4v9ve25zl.us-east-2.rds.amazonaws.com",
     user: "root",
     password: "12345678",
-    database: "myclass"
+    database: "myclass",
+    port: '3306',
 });
 
 connection.connect(function(err) {
